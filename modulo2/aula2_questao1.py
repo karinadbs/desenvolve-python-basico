027d431cd2d18961b03873f347b4833525ce5671
Karina Duarte Bittencourt de Souza <karina.souza@pditabira.com>
2025-03-04 18:01:00 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,8 @@
+var1=5
+var2=2
+
+print(type(var1),type(var2))
+
+var3=var1/var2
+
+print(type(var3),var3)
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,10 @@
+var1="o resultado é"
+var2=10
+var3=3.5
+
+soma=var2+var3
+
+print(type(var1),var1)
+print(type(var2),var2)
+print(type(var3),var3)
+print(type(soma),soma)
