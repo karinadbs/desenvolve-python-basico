50dd849df620b710251753132b36644af0e07804
Karina Duarte Bittencourt de Souza <karina.souza@pditabira.com>
2025-04-04 16:31:42 -0300

    Add files via upload

---
+++ Modulo5/aula1_questao1.py
@@ -0,0 +1,5 @@
+n1= float(input('Digite o primeiro número decimal: '))
+n2= float(input('Digite o segundo número decimal: '))
+diferenca= n1-n2
+diferenca=round(diferenca)
+print ('A diferença absoluta entre os números é:', abs(diferenca))
