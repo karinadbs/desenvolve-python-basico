8a76c44d2c2575c090b26d8762e23c7281ddaea5
Karina Duarte Bittencourt de Souza <karina.souza@pditabira.com>
2025-05-01 20:09:33 -0300

    Add files via upload

---
+++ modulo6/aula4_questao1.py
@@ -0,0 +1,13 @@
+pares= [n for n in range (20,50) if n%2==0]
+print(pares)
+
+lista= [1,2,3,4,5,6,7,8,9]
+quadrado=[n**2 for n in lista]
+print(quadrado)
+
+divisiveis7= [n for n in range(1,100) if n%7==0]
+print (divisiveis7)
+
+par_impar = ['par' if n % 2 == 0 else 'impar' for n in range(0, 30, 3)]
+print(par_impar)
+
